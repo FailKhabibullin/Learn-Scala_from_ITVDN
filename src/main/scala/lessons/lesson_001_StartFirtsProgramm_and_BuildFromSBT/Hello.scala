3636a12b7a5512bd66ce4381ff0a32f45af8470a
Fail <rendelkey@yandex.ru>
2021-12-21 19:05:46 +0300

    Change folders. For practical

--- src/main/scala/lessons/lesson_001_StartFirtsProgramm_and_BuildFromSBT/Hello.scala
+++
@@ -1,5 +0,0 @@
-package lessons.lesson_001_StartFirtsProgramm_and_BuildFromSBT
-
-object Hello extends App {
-
-}
---
+++ src/main/scala/lessons/modules/module_Scala_Start/lesson_001_StartFirtsProgramm_and_BuildFromSBT/Hello.scala
@@ -0,0 +1,5 @@
+package lessons.modules.module_Scala_Start.lesson_001_StartFirtsProgramm_and_BuildFromSBT
+
+object Hello extends App {
+
+}
